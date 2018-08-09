28860a6ae82bc9068ece91dfd0c257967e7eef95
TestDp <Cristianmg13@hotmail.com>
2018-08-09 16:41:13 -0500

    ruta para mostrar el welcome

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
